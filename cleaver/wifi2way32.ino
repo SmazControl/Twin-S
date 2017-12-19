37142570708a0eb5e49db53f029a85ed44634366
SmazControl <smazcontrol@gmail.com>
2017-12-19 20:35:35 +0700

    Update wifi2way32.ino

--- cleaver/wifi2way32.ino
+++ cleaver/wifi2way32.ino
@@ -1,8 +1,9 @@
+// Can't use hang in Wifi Scan 
 #include <ESP32WebServer.h> //https://github.com/nhatuan84/esp32-webserver
 #include <WiFi.h>
 #include <HTTPClient.h>
 
-IPAddress apIP(192, 168, 1, 1);
+IPAddress apIP(192, 168, 4, 1);
 String ssid;
 String password;
 int connect = 0;
@@ -10,10 +11,10 @@ uint64_t chipid;
 
 String newssid;
 String newpassword;
-
-const char* host = "192.168.1.1";
+String host;
 
 ESP32WebServer server(80);
+HTTPClient http;
 
 int count = 0;
 String payload;
@@ -56,6 +57,7 @@ void handleRoot() {
   s += ++count;
   s += "<br>";
   if (connect == 0) {
+    Serial.println("scan start");
     int n = WiFi.scanNetworks();
     Serial.print("scan done");
     if (n == 0) {
@@ -65,7 +67,7 @@ void handleRoot() {
       Serial.println(" networks found");
     }
     for (int i = 0; i<n; ++i) {
-      s += "<a href='http://192.168.1.1/";
+      s += "<a href='http://192.168.4.1/";
       s += WiFi.SSID(i);
       s += "'>";
       s += WiFi.SSID(i);
@@ -78,42 +80,45 @@ void handleRoot() {
 }
 
 void handleNotFound() {
-  Serial.print("request :");
-  Serial.println(server.uri());
-  ssid = String(server.uri()).substring(1);
-  password = String(server.uri()).substring(4);
-  count++;
-
-  if (connect == 0) {
-    payload = "request : "+ssid;
-    WiFi.begin(ssid.c_str(), password.c_str());
-    while (WiFi.status() != WL_CONNECTED) {
-      Serial.print('.');
-      delay(1000);
-    }
-    Serial.print("Connected to: ");
-    Serial.print(WiFi.SSID());
-    Serial.print(", IP address: ");
-    Serial.println(WiFi.localIP());
-    connect = 1;
-  }
-  HTTPClient http;
-  Serial.print("[HTTP]begin...\n");
-  http.begin(host);
-  Serial.print("[HTTP]GET...\n");
-  int httpCode = http.GET();
-  if(httpCode > 0) {
-    // Serial.printf("[HTTP]GET...code: %d\n", httpCode);
-    if(httpCode == HTTP_CODE_OK){
-      payload = http.getString();
-      // Serial.println(payload);
-      int equal = payload.indexOf('=');
-      int value = payload.substring(equal+1).toInt();
-    }
+  if (String(server.uri()) == "/favicon.ico") {
+    
   } else {
-    Serial.printf("[HTTP]GET...failed, error: %s\n",http.errorToString(httpCode).c_str());
-  }
-  http.end();
-  server.send(200, "text/html", payload);
+    Serial.print("request :");
+    Serial.println(server.uri());
+    ssid = String(server.uri()).substring(1);
+    password = String(server.uri()).substring(4);
+    count++;
 
+    if (connect == 0) {
+      payload = "request : "+ssid;
+      WiFi.begin(ssid.c_str(), password.c_str());
+      while (WiFi.status() != WL_CONNECTED) {
+        Serial.print('.');
+        delay(1000);
+      }
+      Serial.print("Connected to: ");
+      Serial.print(WiFi.SSID());
+      Serial.print(", IP address: ");
+      Serial.println(WiFi.localIP());
+      host = "http://"+String(WiFi.localIP());
+      connect = 1;
+    }
+    Serial.print("[HTTP]begin...\n");
+    http.begin(host.c_str());
+    Serial.print("[HTTP]GET...\n");
+    int httpCode = http.GET();
+    if(httpCode > 0) {
+      // Serial.printf("[HTTP]GET...code: %d\n", httpCode);
+      if(httpCode == HTTP_CODE_OK){
+        payload = http.getString();
+        // Serial.println(payload);
+        int equal = payload.indexOf('=');
+        int value = payload.substring(equal+1).toInt();
+      }
+    } else {
+      Serial.printf("[HTTP]GET...failed, error: %s\n",http.errorToString(httpCode).c_str());
+    }
+    http.end();
+    server.send(200, "text/html", payload);
+  }
 }
